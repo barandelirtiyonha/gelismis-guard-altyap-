5176d62cf367758dbd322ad0048582c072de0608
Glitch (stark-code-bos-altyapi) <none>
2020-04-12 15:04:46 +0000

    🚤👩‍❤️‍💋‍👩 Checkpoint ./events/ready.js:11195635/30

--- events/ready.js
+++ events/ready.js
@@ -10,7 +10,7 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("online");
    var oyun = [
-        "Twx Creative",
+        "youtube.com/lord",
 
 
     ];
