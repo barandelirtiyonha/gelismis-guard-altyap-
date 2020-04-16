381710583b65c56d81e0099a0bfdda94e6a07ab8
Glitch (stark-code-bos-altyapi) <none>
2020-04-16 21:20:12 +0000

    💁🍇 Checkpoint ./bot.js:11195635/1572

--- bot.js
+++ bot.js
@@ -384,25 +384,3 @@ client.on("message", async  msg => {
 //----------------------------LİNK ENGEL SON----------------------------------------------------||
 //----------------------------LİNK ENGEL SON----------------------------------------------------||
 //----------------------------LİNK ENGEL SON----------------------------------------------------||
-
-//------------------------------------------ANTİ SPAM-------------------------------------------\\
-//------------------------------------------ANTİ SPAM-------------------------------------------\\
-//------------------------------------------ANTİ SPAM-------------------------------------------\\
-
-const antispam = require("discord-anti-spam-tr");
-antispam(client, {
-  uyarmaSınırı: 4, //Uyarılmadan önce aralıkta gönderilmesine izin verilen maksimum mesaj miktarı.
-  banlamaSınırı: 7, //Yasaklanmadan önce aralıkta gönderilmesine izin verilen maksimum ileti miktar.
-  aralık: 1000, // ms kullanıcılarda zaman miktarı, yasaklanmadan önce aralık değişkeninin maksimumunu gönderebilir.
-  uyarmaMesajı: "Spamı Durdur Yoksa Mutelerim.", // Uyarı mesajı, kullanıcıya hızlı gideceklerini belirten kullanıcıya gönderilir..
-  rolMesajı: "Spam için yasaklandı, başka biri var mı?", //Yasak mesaj, yasaklanmış kullanıcıyı ,Banlar
-  maxSpamUyarı: 8,//Bir kullanıcının uyarılmadan önce bir zaman dilimi içinde gönderebileceği maksimum kopya sayısı
-  maxSpamBan: 12, //Bir kullanıcının yasaklanmadan önce bir zaman diliminde gönderebildiği maksimum kopya sayısı
-  zaman: 7, // Spamdan sonraki zaman
-  rolİsimi: "spam-susturulmuş" // Spam Atan Kullanıcılar Verilecek Röl
-});
-
-
-//------------------------------------------ANTİ SPAM SON-------------------------------------------\\
-//------------------------------------------ANTİ SPAM SON-------------------------------------------\\
-//------------------------------------------ANTİ SPAM SON-------------------------------------------\\
